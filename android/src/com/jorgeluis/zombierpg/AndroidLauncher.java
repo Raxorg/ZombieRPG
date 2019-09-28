0201336ca3020d85ed894b4762066728e5f1221f
Luis Frontanilla <lrdolucho@gmail.com>
2019-09-28 03:16:57 -0400

    Initial commit

---
+++ android/src/com/jorgeluis/zombierpg/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jorgeluis.zombierpg;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jorgeluis.zombierpg.ZombieGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ZombieGame(), config);
+	}
+}
---
+++ desktop/src/com/jorgeluis/zombierpg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jorgeluis.zombierpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jorgeluis.zombierpg.ZombieGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZombieGame(), config);
+	}
+}
